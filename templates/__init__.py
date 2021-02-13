e492a34e0c42ff319aa4a97fec44cbaa8ef2178e
zphyrj <46908127+zphyrj@users.noreply.github.com>
2021-02-12 22:13:04 -0800

    flask

--- templates/__init__.py
+++ templates/__init__.py
@@ -2,7 +2,10 @@ from flask import Flask, render_template, request, jsonify
 import json, numpy as np
 app = Flask(__name__)
 
+@app.route("/")
+def main():
+    return render_template("index.html")
+
 @app.route("/api/v1/getSimilarModels", methods=['GET'])
 def getSimilarModels():
 	points = json.loads(request.args.get('pts', default='[0, 0, 0, 1, 1, 1]'))
-	
