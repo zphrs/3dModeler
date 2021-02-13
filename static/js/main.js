ac61f8d9f85b3e8dbe618c7acf2bd63282a56726
VolVox99 <alirastegar123@gmail.com>
2021-02-12 22:03:06 -0800

    added button changing on click

--- static/js/main.js
+++ static/js/main.js
@@ -2,15 +2,15 @@ const arrow_button = document.getElementById('arrow-button')
 const states = ['fa-arrows-alt-v', 'fa-arrows-alt-h']
 let state = 0
 
-arrow_button.addEventListener('onclick', () => {
+arrow_button.addEventListener('click', () => {
     ++state;
     const new_class = states[state % states.length]
     const old_class = document.getElementById('arrow-button-icon').className
     let current_class;
     states.forEach(state => {
-        if (state in old_class)
+        if (old_class.includes(state))
             current_class = state;
     })
-    arrow_button.getElementById('arrow-button-icon').className = old_class.replace(current_class, new_class)
-
+    document.getElementById('arrow-button-icon').className = old_class.replace(current_class, new_class)
+    console.log('clicked')
 })
