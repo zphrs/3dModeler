f76f9251e3b6ec7a41336f3f7face33d0802483d
zphyrj <46908127+zphyrj@users.noreply.github.com>
2021-02-13 12:29:28 -0800

    raycasting and grid of dots

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -1,12 +1,13 @@
+import * as THREE from './three.module.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
-const material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
 let cubes = [];
 const res = 10;
+const parent = new THREE.Object3D();
 const distBetweenDots = 1;
 for (var x = 0; x<res; x++)
 {
@@ -15,8 +16,10 @@ for (var x = 0; x<res; x++)
 		for (var z = 0; z<res; z++)
 		{
 			const geometry = new THREE.BoxGeometry(.1, .1, .1);
+			const material = new THREE.MeshBasicMaterial( { color: 0x808080 } );
 			const cube = new THREE.Mesh(geometry, material);
-			cube.position.set(x-res/2, y-res/2, z);
+			parent.attach(cube);
+			cube.position.set(x-res/2+.5, y-res/2+.5, z);
 			cube.position.multiplyScalar(distBetweenDots);
 			console.log(cube.position);
 			cubes.push(cube);
@@ -28,10 +31,21 @@ cubes.forEach(e=>
 		scene.add(e);
 	}
 )
-
-camera.position.z = 5;
+// scene.add(parent);
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+camera.position.z = 20;
 function animate() {
 	requestAnimationFrame( animate );
+	raycaster.setFromCamera( mouse, camera );
+	// calculate objects intersecting the picking ray
+	const intersects = raycaster.intersectObjects( scene.children );
+
+	for ( let i = 0; i < intersects.length; i ++ ) {
+
+		intersects[ i ].object.material.color.set( 0xff0000 );
+
+	}
 	renderer.render( scene, camera );
 }
 animate();
@@ -41,4 +55,17 @@ window.addEventListener('resize', e=>
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
-}, false)
+}, false);
+
+window.addEventListener( 'mousemove', onMouseMove, false );
+function onMouseMove( event ) {
+
+	// calculate mouse position in normalized device coordinates
+	// (-1 to +1) for both components
+
+	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+}
+
+window.addEventListener('pointerdown', onPointerDown, false);
