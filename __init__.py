4859e90b3da511ff6962cf42a9bed11631aa51df
zphyrj <46908127+zphyrj@users.noreply.github.com>
2021-02-12 21:45:48 -0800

    deleted

--- __init__.py
+++ __init__.py
@@ -1,9 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import json, numpy as np
 app = Flask(__name__)
-@app.route("/")
-def main():
-    return render_template("test.html")
 
 @app.route("/api/v1/getSimilarModels", methods=['GET'])
 def getSimilarModels():
