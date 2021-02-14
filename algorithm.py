981d2d8c5dee04a18eb6b76ecf5fa933d62d77fe
zphyrj <46908127+zphyrj@users.noreply.github.com>
2021-02-13 23:26:48 -0800

    fixed algo

--- algorithm.py
+++ algorithm.py
@@ -1,8 +1,7 @@
 import numpy as np
 import ai
 def process_points(points):
-    ai.compareModels(points, len(points)/3)
-    return points + [5, 3, 1, -2, 3]
+    return ai.compareModels(points, getModelsInDatabase(len(points)/3))
 
 
 def getModelsInDatabase(ptLength):
