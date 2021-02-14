5df86a8e2100d539ded2e6cac56de080bcceee5b
zphyrj <46908127+zphyrj@users.noreply.github.com>
2021-02-13 16:15:06 -0800

    pyhton

--- __init__.py
+++ __init__.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template, request, jsonify
 import json, numpy as np
 app = Flask(__name__)
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+@app.route("/api/v1/getSimilarModels", methods=['GET'])
+def getSimilarModels():
+	points = json.loads(request.args.get('pts', default='[0, 0, 0, 1, 1, 1]'))
+	output = np.array()
+
+	return output.tolist()
+
+
 
 def normalizePts(npArr):
 
@@ -21,16 +33,4 @@ def compareModels(listOfModels, npArr):
 	# sort the models based on score
 	# return score
 
-
-
-@app.route("/")
-def main():
-    return render_template("index.html")
-
-@app.route("/api/v1/getSimilarModels", methods=['GET'])
-def getSimilarModels():
-	points = json.loads(request.args.get('pts', default='[0, 0, 0, 1, 1, 1]'))
-	output = np.array()
-
-	return output.tolist()
 app.run(debug=True)
